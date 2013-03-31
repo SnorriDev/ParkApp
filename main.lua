785fa7e73422abfe78b012e54abd2144c460845a
viking-sudo-rm <vikingarnir.will@gmail.com>
2013-03-31 10:59:38 -0400

    added menu items and deleted excess files

--- main.lua
+++ main.lua
@@ -11,10 +11,11 @@ storyboard.gotoScene("scenetemplate")
 
 display.setStatusBar(display.DefaultStatusBar)
 
-local scenes = {"scenetemplate","scenetemplate","scenetemplate","scenetemplate","scenetemplate"}
-
 local tabButtons = {
-	{label="1",up="openMenuIcon.jpg",down="openMenuIcon.jpg",onPress=function(event) print("yo") end,selected=true},
+	{label="Map",up="openMenuIcon.jpg",down="openMenuIcon.jpg",onPress=function(event) print("yo") end,selected=true},
+	{label="Events",up="openMenuIcon.jpg",down="openMenuIcon.jpg",onPress=function(event) print("yo") end},
+	{label="Rules",up="openMenuIcon.jpg",down="openMenuIcon.jpg",onPress=function(event) print("yo") end},
+	{label="More",up="openMenuIcon.jpg",down="openMenuIcon.jpg",onPress=function(event) print("yo") end},
 }
 
 local bar = widget.newTabBar{top=display.contentHeight - 50,buttons=tabButtons}
