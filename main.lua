ae6b556d5207aabec338e3c3d39439afd6322de4
unknown <jaseibert@WS113891JaSeibert.packer.edu>
2013-05-29 10:57:02 -0400

    added scroll bar to research

--- main.lua
+++ main.lua
@@ -26,14 +26,14 @@ end
 local function onMenuOptionPressed(event)
 	storyboard.gotoScene(event.target.id, {params={
 		title = "TITLE",
-		content = "Lorem ipsum"
+		content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur imperdiet consectetur euismod. Phasellus non ipsum vel eros vestibulum consequat. Integer convallis quam id urna tristique eu viverra risus eleifend.\n\nAenean suscipit placerat venenatis. Pellentesque faucibus venenatis eleifend. Nam lorem felis, rhoncus vel rutrum quis, tincidunt in sapien. Proin eu elit tortor. Nam ut mauris pellentesque justo vulputate convallis eu vitae metus. Praesent mauris eros, hendrerit ac convallis vel, cursus quis sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum, dui in vehicula dapibus, lorem nisi placerat turpis, quis gravida elit lectus eget nibh. Mauris molestie auctor facilisis.\n\nCurabitur lorem mi, molestie eget tincidunt quis, blandit a libero. Cras a lorem sed purus gravida rhoncus. Cras vel risus dolor, at accumsan nisi. Morbi sit amet sem purus, ut tempor mauris.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur imperdiet consectetur euismod. Phasellus non ipsum vel eros vestibulum consequat. Integer convallis quam id urna tristique eu viverra risus eleifend.\n\nAenean suscipit placerat venenatis. Pellentesque faucibus venenatis eleifend. Nam lorem felis, rhoncus vel rutrum quis, tincidunt in sapien. Proin eu elit tortor. Nam ut mauris pellentesque justo vulputate convallis eu vitae metus. Praesent mauris eros, hendrerit ac convallis vel, cursus quis sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum, dui in vehicula dapibus, lorem nisi placerat turpis, quis gravida elit lectus eget nibh. Mauris molestie auctor facilisis.\n\nCurabitur lorem mi, molestie eget tincidunt quis, blandit a libero. Cras a lorem sed purus gravida rhoncus. Cras vel risus dolor, at accumsan nisi. Morbi sit amet sem purus, ut tempor mauris."
 	}})
 end
 
 local tabButtons = {
 	{label="Map",id="map",up="openMenuIcon.jpg",down="openMenuIcon.jpg",onPress=onMenuOptionPressed,selected=true},
 	{label="Research",id="research",up="openMenuIcon.jpg",down="openMenuIcon.jpg",onPress=onMenuOptionPressed},
-	{label="Rules",id="rules",up="openMenuIcon.jpg",down="openMenuIcon.jpg",onPress=onMenuOptionPressed},
+	{label="Sustainability",id="sustainability",up="openMenuIcon.jpg",down="openMenuIcon.jpg",onPress=onMenuOptionPressed},
 	{label="More",id="more",up="openMenuIcon.jpg",down="openMenuIcon.jpg",onPress=onMenuOptionPressed},
 }
 
--- research.lua
+++ research.lua
@@ -1,19 +1,26 @@
 local storyboard = require("storyboard")
 local scene = storyboard.newScene()
+scrollView = require("scrollView")
 
 function scene:createScene(event)
 	local group = self.view
+	local scrollView = scrollView.new{top=display.screenOriginY, bottom=display.screenOriginY}
 	
 	data = event.params
 	
 	local title = display.newText(data.title, 0, 0, native.systemFontBold, 100)
-	group:insert(title)
+	scrollView:insert(title)
 	if picture ~= nil then
 		local picture = display.newImage(data.picture, 0, 105)
 		group:insert(picture)
 	end
 	local content = display.newText(data.content, 0, 200, native.systemFontBold, 50)
-	group:insert(content)
+	scrollView:insert(content)
+	local scrollBackground = display.newRect(0,0,display.contentWidth, scrollView.height+64)
+	scrollBackground:setFillColor(0,0,0)
+	scrollView:insert(1, scrollBackground)
+	
+	scrollView:addScrollBar()
 end
 
 function scene:enterScene(event)
